f02cbe07ea21bbbec23a805039e62306a6575320
Sean Donato <seanvincentdonato@gmail.com>
2022-09-25 12:07:39 -0400

    Initial Commit

---
+++ figTestiOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  figTestiOS
+//
+//  Created by Sean Donato on 9/25/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
