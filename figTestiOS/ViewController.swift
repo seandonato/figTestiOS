a070913a4e671914ff238462bea510c6cc511e4f
Sean Donato <seanvincentdonato@gmail.com>
2022-10-04 13:16:10 -0400

    message

---
+++ figTestiOS/ViewController.swift
@@ -0,0 +1,32 @@
+import UIKit
+import Foundation
+
+class ViewController: UIViewController{
+	var button268 = UIButton()
+	var button269 = UIButton()
+	override func viewDidLoad(){
+
+		setup()
+		stylize()
+	}
+
+	func setup(){
+
+
+		self.view.addSubview(button268)
+
+		self.view.addSubview(button269)
+	}
+	
+	func stylize(){
+
+
+		button268.backgroundColor = UIColor(red: 0, green: 0.7441667318344116, blue: 0.7916666865348816,alpha:1) 
+		button268.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([button268.heightAnchor.constraint(equalToConstant:86),button268.widthAnchor.constraint(equalToConstant:284),button268.topAnchor.constraint(equalTo: self.view.topAnchor, constant:481),button268.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:39)])
+		button269.backgroundColor = UIColor(red: 0.7416666746139526, green: 0, blue: 0,alpha:1) 
+		button269.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([button269.heightAnchor.constraint(equalToConstant:80),button269.widthAnchor.constraint(equalToConstant:276),button269.topAnchor.constraint(equalTo: self.view.topAnchor, constant:334),button269.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:54)])
+		}
+	
+}
