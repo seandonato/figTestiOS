59acf76b82ccc6c670fc93592a50e359549792ba
Sean Donato <seanvincentdonato@gmail.com>
2022-11-12 18:40:26 -0500

    message

---
+++ figTestiOS/secondViewViewController1112.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Foundation
+
+class secondViewViewController1112 : UIViewController{
+	override func viewDidLoad(){
+
+		setup()
+		stylize()
+	}
+
+	func setup(){
+
+
+		self.view.backgroundColor = UIColor(red: 0.4958333373069763, green: 0.24378472566604614, blue: 0.24378472566604614,alpha:1) 
+	}
+	
+	func stylize(){
+
+
+		}
+	
+}
