2d5c7c68ba6c639a2214e86135e1f73abc8b0685
Sean Donato <seanvincentdonato@gmail.com>
2022-11-12 18:56:35 -0500

    message

---
+++ figTestiOS/thirdvcViewController1215.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Foundation
+
+class thirdvcViewController1215 : UIViewController{
+	override func viewDidLoad(){
+
+		setup()
+		stylize()
+	}
+
+	func setup(){
+
+
+		self.view.backgroundColor = UIColor(red: 1, green: 1, blue: 1,alpha:1) 
+	}
+	
+	func stylize(){
+
+
+		}
+	
+}
