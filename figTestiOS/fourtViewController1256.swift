84b95f5c94d0b62483995506c991efa7ca16595a
Sean Donato <seanvincentdonato@gmail.com>
2022-11-13 12:35:48 -0500

    message

---
+++ figTestiOS/fourtViewController1256.swift
@@ -0,0 +1,22 @@
+import UIKit
+import Foundation
+
+class fourtViewController1256 : UIViewController{
+	override func viewDidLoad(){
+
+		setup()
+		stylize()
+	}
+
+	func setup(){
+
+
+		self.view.backgroundColor = UIColor(red: 1, green: 1, blue: 1,alpha:1) 
+	}
+	
+	func stylize(){
+
+
+		}
+	
+}
