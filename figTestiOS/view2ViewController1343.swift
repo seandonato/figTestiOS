420c17998ec739f49a8c87b06eba21ea10ff93b3
Sean Donato <seanvincentdonato@gmail.com>
2022-11-16 22:53:41 -0500

    message

--- figTestiOS/view2ViewController1343.swift
+++ figTestiOS/view2ViewController1343.swift
@@ -2,6 +2,7 @@ import UIKit
 import Foundation
 
 class view2ViewController1343 : UIViewController{
+	var button1344 = UIButton()
 	override func viewDidLoad(){
 
 		setup()
@@ -11,12 +12,19 @@ class view2ViewController1343 : UIViewController{
 	func setup(){
 
 
+		self.view.addSubview(button1344)
+
 		self.view.backgroundColor = UIColor(red: 0.9833333492279053, green: 0.4793750047683716, blue: 0.4793750047683716,alpha:1) 
 	}
 	
 	func stylize(){
 
 
+		button1344.backgroundColor = UIColor(red: 0.8509804010391235, green: 0.8509804010391235, blue: 0.8509804010391235,alpha:1) 
+		
+		button1344.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([button1344.heightAnchor.constraint(equalToConstant:83),button1344.widthAnchor.constraint(equalToConstant:336),button1344.topAnchor.constraint(equalTo: self.view.topAnchor, constant:373),button1344.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:49)])
+		button1344.layer.cornerRadius = 41
 		}
 	
 }
