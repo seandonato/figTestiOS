1728cdaf955d61206f2a3a5efaf0d3a312091ea5
Sean Donato <seanvincentdonato@gmail.com>
2022-11-17 20:33:42 -0500

    message

--- figTestiOS/InitialViewController162.swift
+++ figTestiOS/InitialViewController162.swift
@@ -23,12 +23,6 @@ class InitialViewController162 : UIViewController{
 
 		self.view.addSubview(text463)
 
-
-		button269.addTarget(self,action: #selector(NavToscreen23ViewController13515), for: .touchUpInside)
-
-
-		buttonb1062.addTarget(self,action: #selector(NavToscreen33ViewController1358), for: .touchUpInside)
-
 		self.view.backgroundColor = UIColor(red: 0, green: 0.10000014305114746, blue: 1,alpha:1) 
 	}
 	
@@ -56,13 +50,5 @@ class InitialViewController162 : UIViewController{
 		text463.translatesAutoresizingMaskIntoConstraints = false
 		NSLayoutConstraint.activate([text463.topAnchor.constraint(equalTo: self.view.topAnchor, constant:143),text463.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:129)])
 		}
-	@objc func NavToscreen23ViewController13515(){
-		var vc = screen23ViewController13515()
-		self.navigationController?.pushViewController(vc, animated:true)
-	}
-	@objc func NavToscreen33ViewController1358(){
-		var vc = screen33ViewController1358()
-		self.navigationController?.pushViewController(vc, animated:true)
-	}
 	
 }
