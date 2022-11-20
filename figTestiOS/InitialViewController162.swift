817b57928b7f79160fbe4c66fe2639db94b983b8
Sean Donato <seanvincentdonato@gmail.com>
2022-11-20 15:37:02 -0500

    message

--- figTestiOS/InitialViewController162.swift
+++ figTestiOS/InitialViewController162.swift
@@ -4,8 +4,8 @@ import Foundation
 class InitialViewController162 : UIViewController{
 	var button269 = UIButton()
 	var textinput4311 = UITextField()
-	var loginimage11422img = UIImage(named:"login-image 1")
-	var loginimage11422 = UIImageView()
+	var loginimage1422img = UIImage(named:"login-image")
+	var loginimage1422 = UIImageView()
 	var buttonb1062 = UIButton()
 	var text1414 = UILabel()
 	override func viewDidLoad(){
@@ -21,7 +21,7 @@ class InitialViewController162 : UIViewController{
 
 		self.view.addSubview(textinput4311)
 
-		self.view.addSubview(loginimage11422)
+		self.view.addSubview(loginimage1422)
 
 		self.view.addSubview(buttonb1062)
 
@@ -29,7 +29,7 @@ class InitialViewController162 : UIViewController{
 
 		self.view.backgroundColor = UIColor(red: 0.9137254953384399, green: 0.6078431606292725, blue: 0.6039215922355652,alpha:1) 
 	
-	loginimage11422.image = loginimage11422img
+	loginimage1422.image = loginimage1422img
 	}
 	
 	func stylize(){
@@ -45,8 +45,8 @@ class InitialViewController162 : UIViewController{
 		textinput4311.translatesAutoresizingMaskIntoConstraints = false
 		NSLayoutConstraint.activate([textinput4311.heightAnchor.constraint(equalToConstant:52),textinput4311.widthAnchor.constraint(equalToConstant:276),textinput4311.topAnchor.constraint(equalTo: self.view.topAnchor, constant:357),textinput4311.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:43)])
 		textinput4311.layer.cornerRadius = 0
-		loginimage11422.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([loginimage11422.heightAnchor.constraint(equalToConstant:198),loginimage11422.widthAnchor.constraint(equalToConstant:216),loginimage11422.topAnchor.constraint(equalTo: self.view.topAnchor, constant:149),loginimage11422.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:73)])
+		loginimage1422.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([loginimage1422.heightAnchor.constraint(equalToConstant:198),loginimage1422.widthAnchor.constraint(equalToConstant:216),loginimage1422.topAnchor.constraint(equalTo: self.view.topAnchor, constant:149),loginimage1422.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:73)])
 		buttonb1062.backgroundColor = UIColor(red: 0.4375, green: 0.47708338499069214, blue: 0.8333333134651184,alpha:1) 
 		buttonb1062.setTitle("Test Button",for: .normal)
 		buttonb1062.translatesAutoresizingMaskIntoConstraints = false
