b30c9199fd1484f5acfb9541fbeee28c8d82b96a
Sean Donato <seanvincentdonato@gmail.com>
2022-11-20 15:35:34 -0500

    message

--- figTestiOS/InitialViewController162.swift
+++ figTestiOS/InitialViewController162.swift
@@ -4,6 +4,8 @@ import Foundation
 class InitialViewController162 : UIViewController{
 	var button269 = UIButton()
 	var textinput4311 = UITextField()
+	var loginimage11422img = UIImage(named:"login-image 1")
+	var loginimage11422 = UIImageView()
 	var buttonb1062 = UIButton()
 	var text1414 = UILabel()
 	override func viewDidLoad(){
@@ -19,11 +21,15 @@ class InitialViewController162 : UIViewController{
 
 		self.view.addSubview(textinput4311)
 
+		self.view.addSubview(loginimage11422)
+
 		self.view.addSubview(buttonb1062)
 
 		self.view.addSubview(text1414)
 
-		self.view.backgroundColor = UIColor(red: 0, green: 0.10000014305114746, blue: 1,alpha:1) 
+		self.view.backgroundColor = UIColor(red: 0.9137254953384399, green: 0.6078431606292725, blue: 0.6039215922355652,alpha:1) 
+	
+	loginimage11422.image = loginimage11422img
 	}
 	
 	func stylize(){
@@ -32,21 +38,23 @@ class InitialViewController162 : UIViewController{
 		button269.backgroundColor = UIColor(red: 0.44583332538604736, green: 1, blue: 0.8337498903274536,alpha:1) 
 		button269.setTitle("Button A",for: .normal)
 		button269.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([button269.heightAnchor.constraint(equalToConstant:80),button269.widthAnchor.constraint(equalToConstant:276),button269.topAnchor.constraint(equalTo: self.view.topAnchor, constant:334),button269.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:47)])
-		button269.layer.cornerRadius = 40
+		NSLayoutConstraint.activate([button269.heightAnchor.constraint(equalToConstant:55),button269.widthAnchor.constraint(equalToConstant:233),button269.topAnchor.constraint(equalTo: self.view.topAnchor, constant:449),button269.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:65)])
+		button269.layer.cornerRadius = 27
 		textinput4311.backgroundColor = UIColor(red: 0.8509804010391235, green: 0.8509804010391235, blue: 0.8509804010391235,alpha:1) 
 		
 		textinput4311.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([textinput4311.heightAnchor.constraint(equalToConstant:52),textinput4311.widthAnchor.constraint(equalToConstant:276),textinput4311.topAnchor.constraint(equalTo: self.view.topAnchor, constant:212),textinput4311.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:47)])
+		NSLayoutConstraint.activate([textinput4311.heightAnchor.constraint(equalToConstant:52),textinput4311.widthAnchor.constraint(equalToConstant:276),textinput4311.topAnchor.constraint(equalTo: self.view.topAnchor, constant:357),textinput4311.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:43)])
 		textinput4311.layer.cornerRadius = 0
+		loginimage11422.translatesAutoresizingMaskIntoConstraints = false
+		NSLayoutConstraint.activate([loginimage11422.heightAnchor.constraint(equalToConstant:198),loginimage11422.widthAnchor.constraint(equalToConstant:216),loginimage11422.topAnchor.constraint(equalTo: self.view.topAnchor, constant:149),loginimage11422.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:73)])
 		buttonb1062.backgroundColor = UIColor(red: 0.4375, green: 0.47708338499069214, blue: 0.8333333134651184,alpha:1) 
 		buttonb1062.setTitle("Test Button",for: .normal)
 		buttonb1062.translatesAutoresizingMaskIntoConstraints = false
-		NSLayoutConstraint.activate([buttonb1062.heightAnchor.constraint(equalToConstant:80),buttonb1062.widthAnchor.constraint(equalToConstant:276),buttonb1062.topAnchor.constraint(equalTo: self.view.topAnchor, constant:463),buttonb1062.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:47)])
-		buttonb1062.layer.cornerRadius = 40
+		NSLayoutConstraint.activate([buttonb1062.heightAnchor.constraint(equalToConstant:54),buttonb1062.widthAnchor.constraint(equalToConstant:233),buttonb1062.topAnchor.constraint(equalTo: self.view.topAnchor, constant:546),buttonb1062.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant:66)])
+		buttonb1062.layer.cornerRadius = 27
 		text1414.text = " HELLO WELCOME TO TAPTA"
 		
-		text1414.font = UIFont.systemFont(ofSize:32)
+		text1414.font = UIFont.systemFont(ofSize:24)
 		
 		text1414.textColor = UIColor(red: 1, green: 1, blue: 1,alpha:1) 
 		text1414.translatesAutoresizingMaskIntoConstraints = false
